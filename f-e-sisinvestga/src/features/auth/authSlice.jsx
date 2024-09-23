69bf5313568ece3dd79e458c55e6432738cf9f55
castilloz671 <158379076+castilloz671@users.noreply.github.com>
2024-09-23 19:06:52 -0400

    Configurando inicio de Sesion

--- f-e-sisinvestga/src/features/auth/authSlice.jsx
+++ f-e-sisinvestga/src/features/auth/authSlice.jsx
@@ -1,70 +1,80 @@
-import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
-import { login, logout, getSession } from '../../services/apiServices';
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import { login, logout, getSession } from "../../services/apiServices";
 
 const initialState = {
-    user: null,
-    token: null,
-    role: null,
-    error: null,
-    status: 'idle',
-  };
-  
-  export const loginUser = createAsyncThunk('auth/loginUser', async (credentials, thunkAPI) => {
+  user: null,
+  token: null,
+  role: null,
+  error: null,
+  status: "idle",
+};
+
+export const loginUser = createAsyncThunk(
+  "auth/loginUser",
+  async (credentials, thunkAPI) => {
     try {
       const response = await login(credentials);
       return response; // Retorna los datos del usuario
     } catch (error) {
-      return thunkAPI.rejectWithValue(error.response?.data || 'Error desconocido');
+      return thunkAPI.rejectWithValue(
+        error.response?.data || "Error desconocido"
+      );
     }
-  });
-  
-  export const logoutUser = createAsyncThunk('auth/logoutUser', async (_, thunkAPI) => {
+  }
+);
+
+export const logoutUser = createAsyncThunk(
+  "auth/logoutUser",
+  async (_, thunkAPI) => {
     try {
       await logout();
     } catch (error) {
-      return thunkAPI.rejectWithValue(error.response?.data || 'Error desconocido');
+      return thunkAPI.rejectWithValue(
+        error.response?.data || "Error desconocido"
+      );
     }
-  });
-  
-  const authSlice = createSlice({
-    name: 'auth',
-    initialState,
-    reducers: {
-      loadSessionFromCookies(state) {
-        const { token, role } = getSession(); // Obtener token y rol de las cookies
-        if (token) {
-          state.token = token;
-          state.role = role;
-        }
-      },
-      clearError(state) {
+  }
+);
+
+const authSlice = createSlice({
+  name: "auth",
+  initialState,
+  reducers: {
+    loadSessionFromCookies(state) {
+      const { token, role } = getSession(); // Obtener token y rol de las cookies
+      if (token) {
+        state.token = token;
+        state.role = role;
+      }
+    },
+    clearError(state) {
+      state.error = null;
+    },
+  },
+  extraReducers: (builder) => {
+    builder
+      .addCase(loginUser.pending, (state) => {
+        state.status = "loading";
+      })
+      .addCase(loginUser.fulfilled, (state, action) => {
+        state.status = "succeeded";
+        state.user = action.payload.user;
+        state.token = action.payload.token;
+        state.role = action.payload.user.role;
         state.error = null;
-      },
-    },
-    extraReducers: (builder) => {
-      builder
-        .addCase(loginUser.pending, (state) => {
-          state.status = 'loading';
-        })
-        .addCase(loginUser.fulfilled, (state, action) => {
-          state.status = 'succeeded';
-          state.user = action.payload.user;
-          state.token = action.payload.token;
-          state.role = action.payload.user.role;
-          state.error = null;
-        })
-        .addCase(loginUser.rejected, (state, action) => {
-          state.status = 'failed';
-          state.error = action.payload;
-        })
-        .addCase(logoutUser.fulfilled, (state) => {
-          state.user = null;
-          state.token = null;
-          state.role = null;
-          state.status = 'idle';
-        });
-    },
-  });
-  
-  export const { loadSessionFromCookies, clearError } = authSlice.actions;
-  export default authSlice.reducer;
+      })
+      .addCase(loginUser.rejected, (state, action) => {
+        state.status = "failed";
+        state.error = action.payload;
+      })
+      .addCase(logoutUser.fulfilled, (state) => {
+        state.user = null;
+        state.token = null;
+        state.role = null;
+        state.status = "idle";
+      });
+  },
+});
+
+export const { loadSessionFromCookies, clearError } = authSlice.actions;
+export default authSlice.reducer;
