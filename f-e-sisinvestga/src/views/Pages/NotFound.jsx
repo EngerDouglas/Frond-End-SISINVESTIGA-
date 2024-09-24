65957368fc848b3dc988711c08a9f2b9601e8cfc
castilloz671 <158379076+castilloz671@users.noreply.github.com>
2024-09-24 12:39:18 -0400

    Paginas de NotFound y Unauthorized.

--- f-e-sisinvestga/src/views/Pages/NotFound.jsx
+++ f-e-sisinvestga/src/views/Pages/NotFound.jsx
@@ -1,9 +1,43 @@
-import React from 'react'
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { useSelector } from 'react-redux';
+import { selectCurrentRole } from '../../features/auth/authSlice';
+import '../../css/Pages/NotAcces.css';
 
 const NotFound = () => {
-  return (
-    <div>NotFound</div>
-  )
-}
+  const role = useSelector(selectCurrentRole);
 
-export default NotFound
+  const getRedirectLink = () => {
+    switch (role) {
+      case 'Administrador':
+        return '/admin';
+      case 'Investigador':
+        return '/invest';
+      default:
+        return '/';
+    }
+  };
+
+  const getRedirectText = () => {
+    switch (role) {
+      case 'Administrador':
+        return 'Volver al Panel de Administración';
+      case 'Investigador':
+        return 'Volver al Panel de Investigador';
+      default:
+        return 'Volver a la Página de Inicio';
+    }
+  };
+
+  return (
+    <div className="page-container container">
+      <h1>Página No Encontrada</h1>
+      <p>Lo sentimos, la página que estás buscando no existe.</p>
+      <Link to={getRedirectLink()} className="nav-link">
+        {getRedirectText()}
+      </Link>
+    </div>
+  );
+};
+
+export default NotFound;
--- f-e-sisinvestga/src/views/Pages/Unauthorized.jsx
+++ f-e-sisinvestga/src/views/Pages/Unauthorized.jsx
@@ -1,9 +1,43 @@
-import React from 'react'
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { useSelector } from 'react-redux';
+import { selectCurrentRole } from '../../features/auth/authSlice';
+import '../../css/Pages/NotAcces.css';
 
 const Unauthorized = () => {
-  return (
-    <div>Unauthorized</div>
-  )
-}
+  const role = useSelector(selectCurrentRole);
 
-export default Unauthorized
+  const getRedirectLink = () => {
+    switch (role) {
+      case 'Administrador':
+        return '/admin';
+      case 'Investigador':
+        return '/invest';
+      default:
+        return '/';
+    }
+  };
+
+  const getRedirectText = () => {
+    switch (role) {
+      case 'Administrador':
+        return 'Volver al Panel de Administración';
+      case 'Investigador':
+        return 'Volver al Panel de Investigador';
+      default:
+        return 'Volver a la Página de Inicio';
+    }
+  };
+
+  return (
+    <div className="page-container container">
+      <h1>Acceso No Autorizado</h1>
+      <p>No tienes permisos para acceder a esta página.</p>
+      <Link to={getRedirectLink()} className="nav-link">
+        {getRedirectText()}
+      </Link>
+    </div>
+  );
+};
+
+export default Unauthorized;
