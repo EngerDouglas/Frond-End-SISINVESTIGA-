a98b0f7bfdd93c7b283696a83fbd93f88dc4e8e5
ICamachoI <wwcamachoww@gmail.com>
2024-09-24 15:51:46 -0400

    Actualize los imports de los componentes

--- f-e-sisinvestga/src/views/Proyectos/ListaProyectos.jsx
+++ f-e-sisinvestga/src/views/Proyectos/ListaProyectos.jsx
@@ -1,6 +1,6 @@
 import React from "react";
-import FiltroBusqueda from "../componentes/FiltroBusqueda";
-import MostrarProyectos from "../componentes/MostrarProyectos";
+import FiltroBusqueda from "./components/GestionProyectos/FiltroBusqueda";
+import MostrarProyectos from "./components/GestionProyectos/MostrarProyectos";
 
 const ListaProyectos = () => {
   return (
