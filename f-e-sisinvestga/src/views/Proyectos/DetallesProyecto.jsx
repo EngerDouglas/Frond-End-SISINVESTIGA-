76b25e376e4103b416638c244d0f1f1dd7613c88
ICamachoI <wwcamachoww@gmail.com>
2024-10-02 01:18:15 -0400

    agregue el componente que muestra la informacion del proyecto

---
+++ f-e-sisinvestga/src/views/Proyectos/DetallesProyecto.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import DatosProyectos from "../../components/GestionProyectos/DatosProyectos";
+
+const AgregarProyecto = () => {
+  return (
+    <DatosProyectos></DatosProyectos>
+  );
+};
+
+export default AgregarProyecto;
