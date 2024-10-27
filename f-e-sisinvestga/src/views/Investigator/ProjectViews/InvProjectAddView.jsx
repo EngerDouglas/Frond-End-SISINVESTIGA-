4b09c23c78247130bbbee20f57b55c2685d4b323
castilloz671 <158379076+castilloz671@users.noreply.github.com>
2024-10-26 23:13:38 -0400

    Creando details para request y evaluations.

--- f-e-sisinvestga/src/views/Investigator/ProjectViews/InvProjectAddView.jsx
+++ f-e-sisinvestga/src/views/Investigator/ProjectViews/InvProjectAddView.jsx
@@ -1,8 +1,11 @@
-import React from 'react'
+import React from 'react';
+import InvAddProject from '../../../components/Investigator/ProjectManagement/InvAddProject'
 
 const InvProjectAddView = () => {
   return (
-    <div>InvProjectAddView</div>
+    <div>
+      <InvAddProject />
+    </div>
   )
 }
 
